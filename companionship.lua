c2acad79835bcad93347d0f2a92bc874ca903b14
Alan Baines <abaines@vt.edu>
2019-12-06 14:40:28 -0500

    Update companionship.lua
    smarter about events, and printing

--- companionship.lua
+++ companionship.lua
@@ -8,13 +8,12 @@ end
 
 local function getLowestPlayerDesiredSpeed()
 	local lowestPlayerDesiredSpeed = 9001 -- It's Over 9000!
+
 	for i, player in pairs(game.connected_players) do
 		local player_desired_speed = player.mod_settings["player_desired_speed"].value
-		if false then
-			game.print(player.name .. "  " .. player_desired_speed)
-		end
 		lowestPlayerDesiredSpeed = math.min(lowestPlayerDesiredSpeed,player_desired_speed)
 	end
+
 	return lowestPlayerDesiredSpeed
 end
 
@@ -30,33 +29,62 @@ local function getNewGameSpeed(numberConnectedPlayers,min_companions,slow_speed_
 end
 
 
-local function companionship(event)
+local function on_runtime_mod_setting_changed(event)
+	local setting = event.setting -- string: The setting name that changed.
+	local setting_type = event.setting_type -- string: The setting type: "runtime-per-user", or "runtime-global".
+
+	if setting ~= "player_desired_speed" then
+		return -- not our setting
+	end
+
 	local min_companions = settings.startup['minimum_number_of_companions'].value
 	local slow_speed_companion = settings.startup['speed_when_below_minimum_number_of_companions'].value
 
-	local player=game.players[event.player_index]
 	local eventName = eventNameMapping[event.name]
 	local numberConnectedPlayers = #game.connected_players
 
-	if eventName == "on_player_joined_game" then
-		player.mod_settings["player_desired_speed"].value = math.min(1,player.mod_settings["player_desired_speed"].value)
-	end
+	local previousSpeed = 0 + game.speed
+	local newSpeed = 0 + getNewGameSpeed(numberConnectedPlayers,min_companions,slow_speed_companion)
 
-	game.speed = getNewGameSpeed(numberConnectedPlayers,min_companions,slow_speed_companion)
+	local msg = "Companionship: "..numberConnectedPlayers .. " / " .. min_companions .. "    Speed: " .. newSpeed
 
-	local msg = "Companionship: "..numberConnectedPlayers .. " / " .. min_companions .. "    Speed: " .. game.speed
-
-	if eventName ~= "on_player_created" then
+	if previousSpeed == newSpeed then
+		-- game.print("Companionship: " .. newSpeed,{r=255,g=255})
+	else
 		game.print(msg,{r=255,g=255})
 	end
 
 	msg = msg .. "    [" .. eventName .."]"
 	log(msg)
+
+	game.speed = newSpeed
 end
 
+script.on_event({
+	defines.events.on_runtime_mod_setting_changed,
+},on_runtime_mod_setting_changed)
+
+
+
+local function resetPlayerDesiredSpeed(event)
+	local player = game.players[event.player_index]
+
+	local previousSpeed = 0 + player.mod_settings["player_desired_speed"].value
+	local newSpeed = 0 + math.min(1,previousSpeed)
+
+	local eventName = eventNameMapping[event.name]
+
+	if newSpeed == previousSpeed then
+		-- game.print("Companionship: No change needed: " .. previousSpeed .. " -> " .. newSpeed .. "   [" .. eventName .. "]",{r=128,g=128,b=128})
+	else
+		game.print("Companionship: Reset Game Speed for: "..player.name .. "   [" .. eventName .. "]",{r=255,g=255})
+	end
+
+	player.mod_settings["player_desired_speed"] = {value = newSpeed}
+	--script.raise_event(defines.events.on_runtime_mod_setting_changed,{setting = "player_desired_speed"})
+end
 
 script.on_event({
-	defines.events.on_player_changed_force,
 	defines.events.on_player_created,
 	defines.events.on_player_died,
 	defines.events.on_player_joined_game,
@@ -64,6 +92,5 @@ script.on_event({
 	defines.events.on_player_left_game,
 	defines.events.on_player_removed,
 	defines.events.on_player_respawned,
-	defines.events.on_runtime_mod_setting_changed,
-},companionship)
+},resetPlayerDesiredSpeed)
 
