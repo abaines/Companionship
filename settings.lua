c74049649b824a3db4143e1765eaf149fd23f6e3
Alan Baines <abaines@vt.edu>
2020-06-27 16:02:01 -0400

    improve setting names and descriptions
    deal with rounding comparison issue in factorio

--- settings.lua
+++ settings.lua
@@ -10,7 +10,7 @@ data:extend({
 	{
 		type = "double-setting",
 		name = "speed_when_below_minimum_number_of_companions",
-		default_value = 1/6, -- 10 Updates Per Second
+		default_value = 0.16666, -- 10 Updates Per Second
 		minimum_value = 0.05,
 		maximum_value = 9001, -- It's Over 9000!
 		setting_type = "startup",
